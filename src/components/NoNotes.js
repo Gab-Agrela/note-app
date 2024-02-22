6a8f30af9962efd92237f0c6756fb96fd9655ad2
Agrela <gabriel.agrela@hotmail.com>
2024-02-22 15:12:46 -0300

    feat: no notes element with icon

--- src/components/NoNotes.js
+++ src/components/NoNotes.js
@@ -1,19 +1,20 @@
 import styled from "styled-components";
+import { CiStickyNote } from "react-icons/ci";
 
 export default function NoNote() {
   return (
     <Container>
+      <CiStickyNote size="38px" />
       <p>No notes</p>
-      <p>Add a new note</p>
     </Container>
   );
 }
 
 const Container = styled.div`
-  border: 1px solid red;
-  display: flex;
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
