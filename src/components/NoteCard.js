1bcc172423a1945a053851b1c8a2b86b2afb25d5
GabrielAgrela <gabriel.agrela@hotmail.com>
2024-02-22 09:20:10 -0300

    feaet: delete note from localStorage

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -3,16 +3,25 @@
 import styled from "styled-components";
 import { CiEdit, CiTrash } from "react-icons/ci";
 
-export default function NoteCard({ title, content }) {
+export default function NoteCard({ title, content, id }) {
+  const handleDelete = ({ target }) => {
+    const { id } = target.parentElement.parentElement;
+    const previousNotes = JSON.parse(localStorage.getItem("notes")) || [];
+    const notesWithoutRemovedId = previousNotes.filter(
+      (note) => note.id !== id
+    );
+    localStorage.setItem("notes", JSON.stringify(notesWithoutRemovedId));
+    window.dispatchEvent(new Event("storage"));
+  };
   return (
-    <NoteContainer>
+    <NoteContainer id={id}>
       <Title>{title}</Title>
       <Content>
         <span>{content}</span>
       </Content>
       <ButtonContainer>
         <CiEdit size="24px" />
-        <CiTrash size="22px" />
+        <CiTrash size="22px" onClick={handleDelete} />
       </ButtonContainer>
     </NoteContainer>
   );
